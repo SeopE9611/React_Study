360d7d75e976add6399083129240aabf997df4f7
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-02-20 18:08:10 +0900

    props로 배열 전달하기

--- 02.React_props/src/App.jsx
+++ 02.React_props/src/App.jsx
@@ -1,12 +1,17 @@
 // App.jsx
 
 import Greeting from "./Greeting";
+import UserList from "./UserList";
 
 function App() {
+  const UserNames = ['스기따라', '죡팡', '힝카인']
   return (
     <>
       <Greeting name="섭이" age={20} job="백수" />
       <Greeting name="행섭" age={21} job="비숍" />
+      <br />
+      <UserList users={UserNames} />
+      <br />
     </>
   )
 }
---
+++ 02.React_props/src/UserList.jsx
@@ -0,0 +1,14 @@
+// UserList.jsx
+
+function UserList({ users }) {
+  return (
+    <ul>
+      {users.map((user, index) => (
+        <li key={index}>{user}</li>
+      ))}
+    </ul>
+  );
+}
+
+
+export default UserList;
