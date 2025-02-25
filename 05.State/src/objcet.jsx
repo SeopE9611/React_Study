b20dcbef2e9dfa0f8e970eee0abb18fc3a643eb5
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-02-25 21:23:21 +0900

    UserProfile 컴포넌트에 도시 필드 추가 및 이름 변경 시 나이 1 증가 기능 구현

--- 05.State/src/objcet.jsx
+++ 05.State/src/objcet.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react"
 
 function UserProfile() {
-  const [user, setUser] = useState({ name: "섭이", age: 20 })
+  const [user, setUser] = useState({ name: "섭이", age: 20, city: "Seoul" })
 
   const updataNmae = () => {
-    // 이름만 변경하면서 기존 객체의 데이터를 유지하는 함수
-    setUser((prevUser) => ({ ...prevUser, name: "행섭" }))
+    setUser((prevUser) => ({ ...prevUser, name: "행섭", age: prevUser.age + 1 })) // 1증가
+
   }
   return (
     <div>
       <h2>사용자 정보</h2>
       <p>이름: {user.name} </p>
       <p>나이:{user.age} </p>
+      <p>도시: {user.city}</p>
       <button onClick={updataNmae}>이름 변경</button>
     </div>
   )
