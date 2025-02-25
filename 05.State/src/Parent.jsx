d1bfe5978cff97eaf409da51257431b6ac4e319a
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-02-25 21:34:50 +0900

    Parent 컴포넌트에서 자식 컴포넌트를 메모이제이션하여 불필요한 렌더링 방지

--- 05.State/src/Parent.jsx
+++ 05.State/src/Parent.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 
 function Parent() {
   const [count, setCount] = useState(0)
@@ -6,7 +6,7 @@ function Parent() {
     <div>
       <h2>부모 컴포넌트</h2>
       <button onClick={() => setCount(count + 1)}>부모 카운트 증가</button>
-      <Child />
+      <MemoizedChild />
     </div>
   )
 }
@@ -15,5 +15,7 @@ function Child() {
   console.log('자식 컴포넌트 렌더링');
   return <h3>자식컴포넌트 입니다.</h3>
 }
+
+const MemoizedChild = memo(Child) // React.memo 사용하여 불필요한 렌더링 방지
 
 export default Parent
