e2ebc3c7e9f4d2f8c8d5b4da7c2d8588675ff520
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-02-28 20:51:45 +0900

    일반 CSS 파일을 적용하는 컴포넌트 추가

---
+++ 07.React_Style/src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+
+import "./App.css" // css 파일 import
+
+function App() {
+  return (
+    <div className="container">
+      <h2 className="title">일반 CSS 파일 적용</h2>
+      <button className="button">버튼</button>
+    </div>
+  )
+}
+export default App
