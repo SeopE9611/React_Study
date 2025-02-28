652a3d055ab6a14406bcf9c5658d3e17de389ab1
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-02-28 21:00:13 +0900

    헤더 컴포넌트 추가 및 스타일 적용

--- 07.React_Style/src/App.jsx
+++ 07.React_Style/src/App.jsx
@@ -1,10 +1,12 @@
 // App.jsx
 
 import "./App.css" // css 파일 import
+import Header from "./Header" // 헤더 컴포넌트 import
 
 function App() {
   return (
     <div className="container">
+      <Header /> {/* Header 컴포넌트 */}
       <h2 className="title">일반 CSS 파일 적용</h2>
       <button className="button">버튼</button>
     </div>
---
+++ 07.React_Style/src/Header.jsx
@@ -0,0 +1,9 @@
+// Header.jsx
+
+import "./Header.css"; // 개별 CSS 파일 import
+
+function Header() {
+  return <header className="header">헤더 영역</header>;
+}
+
+export default Header;
