16a4e42d893e9ef7969160d92a1a79b5f32b2361
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-02-28 17:37:19 +0900

    인라인 스타일 적용 예제2

---
+++ 07.React_Style/src/InlineStyleExample2.jsx
@@ -0,0 +1,14 @@
+function InlineStyleExample2() {
+  return (
+    <div>
+      <h2 style={{ color: "blue", fontSize: '24px', fontWeight: "bold" }}>
+        인라인 스타일 적용 예제
+      </h2>
+      <button style={{ backgroundColor: "black", color: "white", padding: "10px" }}>
+        버튼
+      </button>
+    </div>
+  )
+}
+
+export default InlineStyleExample2
--- 07.React_Style/src/main.jsx
+++ 07.React_Style/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./InlineStyleExample";
+import App from "./InlineStyleExample2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
