2476d4ea81143ecab99aba5b7b0e3fa0369141e2
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-03-04 19:45:48 +0900

    리뷰 페이지에 ReviewCard 및 StarRating 컴포넌트 추가, ReviewList 컴포넌트 수정

---
+++ 08-02.ReviewPage/src/components/ReviewCard.jsx
@@ -0,0 +1,19 @@
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
+}
+
+function ReviewCard({review}) {
+  return(
+    <div style={{ border: '1px solid #ccc', marginBottom: '1rem', padding: '1rem' }}>
+      <img src={review.imgUrl} alt="" style={{width:200, heigth:300, objectFit:"cover", marginRight:20}}/>
+      <h2>{review.title}</h2>
+      <p>{review.rating}</p>
+      <p>{formatDate(review.createdAt)}</p>
+      <p>{review.content}</p>
+      
+      
+    </div>
+  )
+}
+export default ReviewCard;
--- 08-02.ReviewPage/src/components/ReviewList.jsx
+++ 08-02.ReviewPage/src/components/ReviewList.jsx
@@ -1,7 +1,18 @@
-function ReviewList() {
+import ReviewCard from './ReviewCard'
+
+// ReviewList 컴포넌트 정의, reviews 배열을 props로 받음
+function ReviewList({reviews}) {
   // console.log(items);
 
-  return <ul>리뷰 목록</ul>;
+  return (
+    <div>
+      {/* map 함수를 사용하여 reviews 배열의 각 요소에 대해 ReviewCard 컴포넌트를 렌더링 */}
+      {reviews.map((review) => (
+        // 각 ReviewCard 컴포넌트에 고유한 key와 review props를 전달
+        <ReviewCard key ={ review.id } review={review} /> 
+      ))}
+    </div>
+  )
 }
 
 export default ReviewList;
