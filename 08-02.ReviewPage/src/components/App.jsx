f780c5841b2449dfedadecdeb9f0abdf262cf476
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-03-04 21:09:25 +0900

    App.jsx에 리뷰 수정 함수를 추가하고 ReviewList에 onEdit props 전달

--- 08-02.ReviewPage/src/components/App.jsx
+++ 08-02.ReviewPage/src/components/App.jsx
@@ -9,7 +9,17 @@ function App() {
   const handleDeleteReview = (id) => { // 삭제할 리뷰의 id를 전달받음
     console.log('삭제할 리뷰 id', id); 
     setReviews(reviews.filter(review => review.id !== id)); // id와 일치하는 리뷰를 제외한 새로운 배열로 리뷰 데이터 업데이트
-    
+  }
+
+  // 리뷰 수정 함수
+  const handleEditReview = (updateReview) => { // 수정할 리뷰 데이터를 전달받음
+    console.log('수정된 리뷰:', updateReview);
+    // id가 일치하는 리뷰를 찾아 수정된 리뷰로 업데이트
+    setReviews(
+      reviews.map((review) => // map 함수를 사용하여 리뷰 데이터를 순회
+        review.id === updateReview.id ? updateReview : review // id가 일치하면 수정된 리뷰로 업데이트, 아니면 기존 리뷰 유지
+      )
+    )
   }
   return (
     <div>
@@ -17,7 +27,7 @@ function App() {
       {/* 리뷰 작성폼이 들어갈 자리 */}
       <ReviewForm />
       {/* 리뷰 리스트 컴포넌트가 들어갈 자리 */}
-      <ReviewList reviews={reviews} onDelete={handleDeleteReview}/> {/* 리뷰 데이터와 리뷰 삭제 함수를 props로 전달 */}
+      <ReviewList reviews={reviews} onDelete={handleDeleteReview} onEdit={handleEditReview}/> {/* 리뷰 데이터와 리뷰 삭제 함수를 props로 전달 */}
     </div>
   );
 }
