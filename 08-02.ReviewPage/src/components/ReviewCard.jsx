f490af185c2d320ecf67fac7448f16a660258be0
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-03-04 20:32:41 +0900

    ReviewCard 컴포넌트에 리뷰 삭제 함수를 props로 추가하고 삭제 버튼 구현

--- 08-02.ReviewPage/src/components/ReviewCard.jsx
+++ 08-02.ReviewPage/src/components/ReviewCard.jsx
@@ -3,7 +3,7 @@ function formatDate(value) { // 날짜 포맷 함수
   return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일 ${date.getHours()}시 ${date.getMinutes()}분 ${date.getSeconds()}초`; // 년, 월, 일, 시, 분, 초 반환
 }
 
-function ReviewCard({ review }) {
+function ReviewCard({ review, onDelete }) { // 리뷰 데이터와 리뷰 삭제 함수를 props로 전달받음
   return (
     <div style={{ border: '1px solid #ccc', marginBottom: '1rem', padding: '1rem' }}>
       <img src={review.imgUrl} alt="" style={{ width: 200, height: 300, objectFit: "cover", marginRight: 20 }} />
@@ -14,6 +14,7 @@ function ReviewCard({ review }) {
       {/* toLocaleString()을 사용하여 브라우저 로케일에 맞게 날짜와 시간을 포맷 */}
       {/* <p>작성일: {new Date(review.createdAt).toLocaleString()}</p> */}
       <p>{review.content}</p>
+      <button onClick={() => onDelete(review.id)}>삭제</button> {/* 리뷰 삭제 함수 호출 */}
     </div>
   );
 }
