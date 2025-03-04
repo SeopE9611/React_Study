68de0e851f0541efb0cee577f2843958d4c29b3a
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-03-04 20:21:35 +0900

    App.jsx에서 삭제기능 구현 (현재 reviews 상태를 관리하고 있는 곳이 App.jsx이므로..)

--- 08-02.ReviewPage/src/components/App.jsx
+++ 08-02.ReviewPage/src/components/App.jsx
@@ -4,14 +4,20 @@ import ReviewForm from './ReviewForm';
 import { useState } from 'react';
 function App() {
   const [reviews, setReviews] = useState(mokData); // 리뷰 데이터 관리
+
+  // 리뷰 삭제 함수
+  const handleDeleteReview = (id) => { // 삭제할 리뷰의 id를 전달받음
+    console.log('삭제할 리뷰 id', id); 
+    setReviews(reviews.filter(review => review.id !== id)); // id와 일치하는 리뷰를 제외한 새로운 배열로 리뷰 데이터 업데이트
+    
+  }
   return (
     <div>
-      {/* <ReviewList items={mokData} /> items 프로퍼티에 mock 데이터 전달 */}
       <h1>영화 리뷰</h1>
       {/* 리뷰 작성폼이 들어갈 자리 */}
       <ReviewForm />
       {/* 리뷰 리스트 컴포넌트가 들어갈 자리 */}
-      <ReviewList reviews={reviews} />
+      <ReviewList reviews={reviews} onDelete={handleDeleteReview}/>
     </div>
   );
 }
