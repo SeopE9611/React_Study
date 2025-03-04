9dd7d9ad6d42a351275583fa4c8c778d7c691ec7
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-03-04 21:10:33 +0900

    ReviewList 컴포넌트에 onEdit props 추가 및 ReviewCard에 전달

--- 08-02.ReviewPage/src/components/ReviewList.jsx
+++ 08-02.ReviewPage/src/components/ReviewList.jsx
@@ -1,7 +1,7 @@
 import ReviewCard from './ReviewCard'
 
 // ReviewList 컴포넌트 정의, reviews 배열을 props로 받음
-function ReviewList({reviews, onDelete}) { // 리뷰 데이터와 리뷰 삭제 함수를 props로 전달받음
+function ReviewList({reviews, onDelete, onEdit}) { // 리뷰 데이터와 리뷰 삭제 함수를 props로 전달받음
   // console.log(items);
 
   return (
@@ -9,7 +9,7 @@ function ReviewList({reviews, onDelete}) { // 리뷰 데이터와 리뷰 삭제 
       {/* map 함수를 사용하여 reviews 배열의 각 요소에 대해 ReviewCard 컴포넌트를 렌더링 */}
       {reviews.map((review) => (
         // 각 ReviewCard 컴포넌트에 고유한 key와 review props를 전달
-        <ReviewCard key ={ review.id } review={review} onDelete={onDelete}/>  // 리뷰 데이터와 리뷰 삭제 함수를 props로 전달
+        <ReviewCard key ={ review.id } review={review} onDelete={onDelete} onEdit={onEdit}/> // 리뷰 데이터와 리뷰 삭제 , 수정 함수를 props로 전달
       ))}
     </div>
   )
